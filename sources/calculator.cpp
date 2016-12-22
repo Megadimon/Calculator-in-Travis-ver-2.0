01c14ac418e7c66f56be27a06966729bb709403d
Дмитрий Соловьёв <md.65@mail.ru>
2016-12-22 03:43:19 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,52 +1,86 @@
-double sum ( float a,  float b)
+double sum ( float a, float b )
 {
-	return a + b;
+return a+b; 
 }
-double sub ( float a,  float b)
-{
-	return a - b;
+
+double sub (float a, float b)
+
+{ 
+return a-b;
 }
-double mult ( float a,  float b)
+
+double mult (float a, float b)
 {
-	return a * b;
+	return a*b;
 }
-double div ( float a,  float b)
+
+double *div (float a, float b, int* c)
 {
-	return (float)a / b;
+	if (b != 0 && a != 0) return (float)a/b;
+		else if (a == 0 && b != 0) return 0;
+			else if (b == 0) 
+			{
+				*c = 1;
+				return nullptr;
+			}
 }
-double pow ( float a )
+
+float pow (float a, float b, int* c)
 {
-	unsigned int extent = 4;
 	float a2 = a;
 	
-	if ( extent != 0 ) 
+	if (a != 0)
 	{
-			for (int i = 1; i < extent; i++)
+		*c = 0;
+		if (b < 0)
 		{
-			a = a * a2;
+	    	for (int i = 1; i <-b ; i++)
+		 	{
+		 	a*=a2;
+		 	}
+			a=(float)1/a;
+			}
+				else if (b>0)
+				{
+					for (int i = 1; i < b; i++)
+					{
+						a*=a2;
+					}
+				}
+		else a == 1.0;
+		return (double) a;
 		}
+		else if ( a == 0 && b < 0 ) *c = 1;
+			else if ( a == 0 && b > 0 ) return 0;
+				else if ( a == 0 && b == 0 ) return 1;
 	}
-		else a = 1.0;
+
+
+double *sq (float a, int* c)
+{	
+	float n = 1.0;  
+	float t = 2.0;
 	
-	return (double) a;
-}
-double sq ( float a )
-{
-	float xn = 1.0;  
-	float xn1 = 2.0;
-
-	if ( a != 1 && a != 0 )
+	if ( a >= 0 )
 	{
-		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
+		*c = 0;
+		if ( a != 1 && a != 0 )
 		{
-		 xn = xn1;
-		 xn1 = (float)(xn + (float)a/xn)/2;
+			while ( (t - n) > 0.000001 || (t - n) < -0.000001  )
+			{
+		 		n = t;
+		 		t = (float)(n + (float)a/n)/2;
+			}	
+
 		}
-
+			else if ( a == 1 ) t = 1;
+				else if ( a == 0 ) t = 0;
+				return t;
 	}
-		else if ( a == 1 ) xn1 = 1;
-			else if ( a == 0 ) xn1 = 0;
-
-
-	return xn1;
+		else 
+		{
+			*c = 1;
+	   		return nullptr;
+		}
+		
 }
