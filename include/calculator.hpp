b175161c031a2d712e6d0ad53cdddf2c7a1d5fac
Дмитрий Соловьёв <md.65@mail.ru>
2016-12-22 10:57:58 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -2,5 +2,5 @@ double sum ( float a,  float b);
 double sub ( float a,  float b);
 double mult ( float a,  float b);
 double *div ( float a,  float b, int* c );
-double pow ( float a, int* extent, int* c);
+double pow ( float a, float b, int* c);
 double *sq ( float a, int* c );
